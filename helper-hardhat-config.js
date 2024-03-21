bc480ec29241f76059b8a3a7ffbe76c050a2dfdc
cqlyj <luoyingjie0721@gmail.com>
2024-03-22 03:21:05 +0800

    Add helper-hardhat-config.js file

---
+++ helper-hardhat-config.js
@@ -0,0 +1,17 @@
+const networkConfig = {
+  31337: {
+    name: "FileTokenTest",
+    symbol: "FTT",
+  },
+  11155111: {
+    name: "FileToken",
+    symbol: "FT",
+  },
+};
+
+const developmentChains = ["hardhat", "localhost"];
+
+module.exports = {
+  networkConfig,
+  developmentChains,
+};
