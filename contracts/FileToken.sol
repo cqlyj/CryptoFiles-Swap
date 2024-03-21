4e2e5f53276de88f7d6d5fae1802f9313996c01f
cqlyj <luoyingjie0721@gmail.com>
2024-03-21 16:10:11 +0800

    Implement basic FileToken contract functions

--- contracts/FileToken.sol
+++ contracts/FileToken.sol
@@ -1,13 +1,75 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.7;
 
-// mint NFT for each file
-// allow owner to define the name and symbol of the token
-// allow owner to set the price of the token
-// After uploading a file to IPFS through the frontend, the URL should be passed to the smart contract to mint the NFT.
-// Access control: only the owner of the file can mint the NFT for the file.
-// The owner of the file can transfer the ownership of the file to another address.
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract FileToken {
-    constructor() {}
+contract FileToken is ERC721URIStorage, Ownable {
+    //error
+    error FileToken__MoreEthNeeded(uint256 requiredAmount, uint256 sentAmount);
+    error FileToken__WithdrawFailed();
+    error FileToken__NoBalanceForWithdraw();
+    error FileToken__InvalidMintFee(uint256 newMintFee);
+
+    //event
+    event MintFeeChanged(uint256 newMintFee);
+    event TokenMinted(
+        address indexed to,
+        uint256 indexed tokenID,
+        string tokenURI
+    );
+    // variables
+
+    uint256 private tokenID;
+    uint256 private mintFee;
+
+    // constructor
+
+    constructor(
+        string memory name,
+        string memory symbol
+    ) ERC721(name, symbol) Ownable(msg.sender) {
+        tokenID = 0;
+        mintFee = 0.01 ether;
+    }
+
+    // functions
+
+    function mintNFT(address _to, string memory _tokenURI) public payable {
+        if (msg.value < mintFee) {
+            revert FileToken__MoreEthNeeded(mintFee, msg.value);
+        }
+        _safeMint(_to, tokenID);
+        _setTokenURI(tokenID, _tokenURI);
+        emit TokenMinted(_to, tokenID, _tokenURI);
+        tokenID++;
+    }
+
+    function changeMintFee(uint256 newMintFee) public onlyOwner {
+        if (newMintFee <= 0) {
+            revert FileToken__InvalidMintFee(newMintFee);
+        }
+        mintFee = newMintFee;
+        emit MintFeeChanged(newMintFee);
+    }
+
+    function withdraw() public onlyOwner {
+        if (address(this).balance == 0) {
+            revert FileToken__NoBalanceForWithdraw();
+        }
+        (bool success, ) = payable(owner()).call{value: address(this).balance}(
+            ""
+        );
+        if (!success) {
+            revert FileToken__WithdrawFailed();
+        }
+    }
+
+    function getMintFee() public view returns (uint256) {
+        return mintFee;
+    }
+
+    function getTokenID() public view returns (uint256) {
+        return tokenID;
+    }
 }
