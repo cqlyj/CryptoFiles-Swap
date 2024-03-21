644a7b0a27471fa72af5911e01267d0855ee39f6
cqlyj <luoyingjie0721@gmail.com>
2024-03-22 03:20:58 +0800

    Add hardhat-deploy, hardhat-verify, and dotenv dependencies, configure solidity version, add sepolia network, gas reporter, etherscan API key, and named accounts

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,38 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("hardhat-deploy");
+require("@nomicfoundation/hardhat-verify");
+require("dotenv").config();
+
+const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+const SEPOLIA_ETHERSCAN_API_KEY = process.env.SEPOLIA_ETHERSCAN_API_KEY;
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.24",
+  solidity: {
+    compilers: [{ version: "0.8.24" }],
+  },
+  defaultNetwork: "hardhat",
+  networks: {
+    sepolia: {
+      url: SEPOLIA_RPC_URL,
+      accounts: [PRIVATE_KEY],
+      chainId: 11155111,
+      blockConfirmations: 6,
+    },
+  },
+  gasReporter: {
+    enabled: true,
+  },
+  etherscan: {
+    apiKey: SEPOLIA_ETHERSCAN_API_KEY,
+  },
+  namedAccounts: {
+    deployer: {
+      default: 0,
+    },
+    user: {
+      default: 1,
+    },
+  },
 };
