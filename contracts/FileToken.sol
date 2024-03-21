c32e518da789b2c8d165c58b5232cf1bc84283a5
cqlyj <luoyingjie0721@gmail.com>
2024-03-21 14:40:45 +0800

    set up FileToken contract and specify what to implement

---
+++ contracts/FileToken.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+
+// mint NFT for each file
+// allow owner to define the name and symbol of the token
+// allow owner to set the price of the token
+// After uploading a file to IPFS through the frontend, the URL should be passed to the smart contract to mint the NFT.
+// Access control: only the owner of the file can mint the NFT for the file.
+// The owner of the file can transfer the ownership of the file to another address.
+
+contract FileToken {
+    constructor() {}
+}
