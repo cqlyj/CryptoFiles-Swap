9dbd9b5d1f66ad87943a842023b574194afcd52a
cqlyj <luoyingjie0721@gmail.com>
2024-04-02 12:40:33 +0800

    Add IFileToken interface

---
+++ contracts/interfaces/IFileToken.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+
+interface IFileToken {
+    // Function to mint an NFT for a file
+    function mintNFT(address _to) external payable;
+
+    // Function to change the minting fee
+    function changeMintFee(uint256 newMintFee) external;
+
+    // Function to withdraw the contract's balance
+    function withdraw() external;
+
+    // Function to get the token URI of a minted NFT
+    function getFileTokenURI(
+        uint256 _tokenId
+    ) external view returns (string memory);
+}
